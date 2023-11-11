24603105260f1d2223668a40f301a9344f126ee5
Irinaka Robdone <irinaka.robodone@gmail.com>
2023-11-11 19:29:09 +0900

    init commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+left = Motor(Port.B)
+right = Motor(Port.C)
+robot = DriveBase(left, right, 55, 120)
+color_sensor = ColorSensor(Port.S3)
+robot.straight(230)
+robot.turn(90)
+while not ColorSensor
