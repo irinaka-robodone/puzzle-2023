22cb7901beae0e41540088c2947657cbed1fa654
passive-radio <srccreator@gmail.com>
2023-11-25 19:07:15 +0900

    add board

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pyxel
+import random
 
 class App:
     def __init__(self):
@@ -6,19 +7,44 @@ class App:
         self.height = 160
         self.state = "start"
         pyxel.init(self.width, self.height)
+        self.board = self.create_solvable_puzzle()
+        self.tile_size = 10
+        pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
         
     def update(self):
+        if not self.state == "play" and pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            self.state = "play"
+            
+    def draw(self):
+        pyxel.cls(0)
         if self.state == "start":
-            self.draw.start()
-        if self.
+            self.draw_start()
         if self.state == "start":
             self.draw_start()
         if self.state == "play":
-            self.draw_play()
-    
+            # self.draw_play()
+            self.draw_bodo()
+            
     def draw_start(self):
         pyxel.text(50, 60, "Click to start", pyxel.frame_count % 16)
-        
     
+    def draw_bodo(self):
+        for y, row in enumerate(self.board):
+            for x, tile in enumerate(row):
+                if tile != 0:
+                    pyxel.rect(x * self.tile_size, y * self.tile_size, self.tile_size, self.tile_size, 11)
+                    pyxel.text(x * self.tile_size + 5, y * self.tile_size + 5, str(tile), 7)
+                    
+    def is_solvable(self, numbers):
+        return True
+    
+    def create_solvable_puzzle(self):
+        numbers = list(range(1, 9)) + [0]  # 1から8と空きスペースの0
+        while True:
+            random.shuffle(numbers)
+            if self.is_solvable(numbers):
+                break
+        return [numbers[i:i+3] for i in range(0, 9, 3)]
+
 App()
